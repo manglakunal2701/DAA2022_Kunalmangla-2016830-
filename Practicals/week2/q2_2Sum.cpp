dadc3159e02591f64e6c205fceb33cc006d8f241
manglakunal2701 <72389555+manglakunal2701@users.noreply.github.com>
2022-04-18 00:49:26 +0530

    Update q2_2Sum.cpp

--- Practicals/week2/q2_2Sum.cpp
+++ Practicals/week2/q2_2Sum.cpp
@@ -1,37 +1,36 @@
 #include<iostream>
-#include <vector>
 using namespace std;
-void sum2(int arr[],int n,int target){
-    int low=0;
-    int high=n-1;
-    int sum=0;
-    while(low<high){
-        sum=arr[low]+arr[high];
-        if(sum==target){
-            cout<<"i is "<<low<<" j is "<<high<<endl;
-            break;
-        }
-        else if(sum>target)
-        {
-            high--;
-        }
-        else{
-            low++;
+void sum2(int arr[],int n){
+    bool res= false;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                if(arr[i]+arr[j]==arr[k]){
+                    res = true;
+                    int a= i+1;
+                    int b=j+1;
+                    int c=k+1;
+                    if(a<b && b<c){
+                    cout<<" result is "<<a<<" "<<b<<" "<<c<<endl;
+                    break;
+                    }
+                }
+            }
         }
     }
+    if(res==false){
+        cout<<" no sequence found"<<endl;
+    }
 }
-
 int main()
 {
    int n;
     cout<<"Enter the number of elements"<<endl;
     cin>>n;
     int arr[n];
+    cout<<"Enter the sorted array"<<endl;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"Enter the key"<<endl;
-    int key;
-    cin>>key;
-    sum2(arr,n,key);    
+    sum2(arr,n);    
 }
