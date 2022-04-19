77bb557f29ea5a5809f0c667100ddf010fbd948a
manglakunal2701 <72389555+manglakunal2701@users.noreply.github.com>
2022-04-19 15:32:03 +0530

    Update q2.cpp

--- Practicals/week4/q2.cpp
+++ Practicals/week4/q2.cpp
@@ -1,43 +1,43 @@
 #include<iostream>
 using namespace std;
-void quickSort(int a[],int l,int r)
-{
-      int i=l,j=r;
-      int key=a[l],t=0;
-      if(l>r)
-      return;
-      while(i<j)
-      {
-          while(key>=a[i]&&i<j)
-          i++;
-          while(key<a[j])
-          j--;
-          if(i<j)
-          {
-              t=a[i];
-              a[i]=a[j];
-              a[j]=t;
-          }
+int counter =0;
+int partition(int array[],int start,int end){
+      int pivot = array[end];
+    int i = start - 1;
 
-      }
-      a[l]=a[j];
-      a[j]=key;
-      quickSort(a,l,j-1);
-      quickSort(a,j+1,r);
-
+    for(int j = start; j <= end - 1; j++){
+        counter++;
+        if (array[j] <= pivot){
+            i = i + 1;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    int temp = array[i+1];
+    array[i+1] = array[end];
+    array[end] = temp;
+    return i + 1;
 }
-
+void quickSort(int arr[],int start,int end){
+    if(start<end){
+        int pivot = partition(arr,start,end);
+        quickSort(arr,start,pivot-1);
+        quickSort(arr,pivot+1,end);
+    }
+}
 int main(){
-   int n;
+    int n;
     cout<<"Enter the number of elements"<<endl;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-   quickSort(arr,0,n-1);
-   for(int i=0;i<n;i++){
-       cout<<arr[i]<<" ";
-   }
+    quickSort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl<<counter;
 
 }
